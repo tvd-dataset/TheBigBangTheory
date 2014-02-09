85ba2f15e533bb4b3d0ffd2898de83a3524674fa
Anindya Roy <roy@limsi.fr>
2014-02-09 14:42:28 +0100

    Updated.

--- TheBigBangTheory/__init__.py
+++ TheBigBangTheory/__init__.py
@@ -26,6 +26,11 @@
 # SOFTWARE.
 #
 
+# Comments (AR):
+# Q1: Why not keep scene/location description in addition to event info? Note that this may be specific to TBBT.
+# Q2: Why not 'event' and 'location', instead of just 'speech'?
+# Q3: Continuous graph?
+
 from tvd.series.plugin import SeriesPlugin
 import re
 import urllib3
@@ -34,6 +39,8 @@ import networkx as nx
 
 
 class TheBigBangTheory(SeriesPlugin):
+
+    
 
     def outline(self, url, source=None, episode=None):
         """
@@ -53,123 +60,81 @@ class TheBigBangTheory(SeriesPlugin):
         """
 
         http = urllib3.PoolManager()
-        r = http.request('GET', url)
+	r = http.request('GET', url)
+	r = r.data
+	r = re.sub('<script[^<]+</script>', '', r)
+ 	r = re.sub('<style[^<]+</style>', '', r)
+	r = re.sub('<div[^<]+</div>', '', r)
+ 	r = re.sub('<[^>]+>', '', r)
+	r = r.split('\n')
 
-        start_scene = 0
-        nb_scene = 0
-        word = r.data.split()
+	text = [] # Text for episode outline.
+	start = 0
+	for line in r:
+		if re.search('\A[ \t\n\r]*\Z', line):
+			continue
+		if re.search('\A[ \t]*episode outline[ \t]*\Z', line, re.IGNORECASE):
+			start = 1
+			continue
+		if start == 1:
+			if re.search('\A[ \t]*resources[ \t]*\Z', line, re.IGNORECASE) or re.search('\A\[*[0-9]*\]*timeline[ \t]*\Z', line, re.IGNORECASE) or re.search('\A[ \t]*commentary and trivia[ \t]*\Z', line, re.IGNORECASE) or re.search('\A[ \t]*trivia[ \t]*\Z', line, re.IGNORECASE):
+				break
+			if not re.search('\A[ \t]*[IVXLCM]+[\.:]+', line): # Remove location description.
+				text.append(' '.join(line.split())) # Keep just events. Q. Why not keep location info?
 
         g = nx.MultiDiGraph(uri=str(episode), source=source)
-
-        sp = ""
-
-        for w in word:
-            if start_scene == 1:
-                sp = sp + " " + str(w)
-            if (re.match("<h2><span", w) or re.match("NewPP", w)) and (nb_scene > 0):
+	nEvents = len(text)
+        for eNo in xrange(nEvents):
                 t1 = T()
                 t2 = T()
-                usp = sp.decode("utf-8")
-                sp = usp.encode("ascii", "ignore")
-                sp = re.sub(re.compile('<i>', re.I), "", sp)
-                sp = re.sub(re.compile('</i>', re.I), "", sp)
-                sp = re.sub(re.compile('<a href=\"/wiki/\S{1,15}(_\(*\S{1,15}\)*)*" title=\"\S{1,15}(\s\(*\S{1,15}\)*)*\">',re.I), "", sp)
-                sp = re.sub(re.compile('<a href=\"/wiki/\S{1,15}(_\(*\S{1,15}\)*)*" title=\"\S{1,15}(\s\(*\S{1,15}\)*)*\" class=\"mw-redirect\">',re.I), "", sp)
-                sp = re.sub(re.compile('<figure class=\"(.*)</figure>',re.I), "", sp)
-                sp = re.sub(re.compile('</a>',re.I), "", sp)
-                sp = re.sub(re.compile('<!-- NewPP',re.I), "", sp)
-                sp = re.sub(re.compile('<p>',re.I), "", sp)
-                sp = re.sub(re.compile('</p>',re.I), "", sp)
-                sp = re.sub(re.compile('<h2><span',re.I), "", sp)
-                g.add_edge(t1, t2, scene="Scene_"+str(nb_scene), speech=sp)
-                sp = ""
-                start_scene = 0
-            if nb_scene > 0 and start_scene == 0 and re.match("<p>", w):
-                sp = sp + " " + str(w)
-                start_scene = 1
-            if re.match("id=\"Scene_(.*)\">Scene", w):
-                nb_scene = nb_scene + 1
+		event = text[eNo]
+                g.add_edge(t1, t2, scene="Scene_"+str(eNo), speech=event) # Why not 'event' instead of 'speech'?
 
         return g
 
     def manual_transcript(self, url, source=None, episode=None):
 
-        http = urllib3.PoolManager()
-        r = http.request('GET', url)
+	#names_map = {'Raj':'Rajesh', 'raj':'rajesh', 'Lesley':'Leslie', 'lesley':'leslie'}
+      	http = urllib3.PoolManager()
+	r = http.request('GET', url)
+	r = r.data
+	r = re.sub('<script[^<]+</script>', '', r)
+ 	r = re.sub('<style[^<]+</style>', '', r)
+	r = re.sub('<div[^<]+</div>', '', r)
+ 	r = re.sub('<[^>]+>', '', r)
+	r = r.split('\n')
 
-        start_scene = 0
-        nb_scene = 0
-        word = r.data.split()
+	g = nx.MultiDiGraph(uri=str(episode), source=source)
 
-        g = nx.MultiDiGraph(uri=str(episode), source=source)
+	for line in r:
+		if re.search('\A[ \t\n\r]*\Z', line): # Empty line.
+			continue
 
-        sp = ""
+		if re.match('\A\s*[Ss]cene\s*[\.:]', line): # Scene description. 
+			continue
 
-        for w in word:
-            if start_scene == 1 and re.match("</div>", w):
-                start_scene = 0
-            if start_scene == 1:
-                nom = (re.search('/>[A-Z][a-z]{1,15} :', w) or (re.search('/>[A-Z]{1,15}:', w)))
-                if nom:
-                    w = "<START>" + nom.group(0) + ":"
-                else:
-                    nom = (re.search('/>[A-Z][a-z]{1,15}', w) or (re.search('/>[A-Z]{1,15}', w)))
-                    if nom:
-                        w = "<START>" + nom.group(0)
-                sp = sp + " " + str(w)
-            if re.match("class=\"postbody\">", w):
-                start_scene = 1
+		if re.search('\A[ \t]*Written by', line, re.IGNORECASE) or re.search('\A[ \t]*Teleplay:', line, re.IGNORECASE) or re.search('\A[ \t]*Story:', line, re.IGNORECASE) or re.search('\A[ \t]*Like this:', line, re.IGNORECASE):
+			break
+		
+		speaker = re.match('\A\s*[^:\.]+\s*:', line)
+		if speaker == None:
+			continue # Comments e.g. '(They begin to fill out forms.)
 
-        sp = re.sub(re.compile('<br />', re.I), "", sp)
-        sp = re.sub(re.compile('<br <START>/>', re.I), "####", sp)
-        sp = re.sub(re.compile('::', re.I), ":", sp)
-        sp = re.sub(re.compile('&quot;', re.I), "", sp)
-        sp = re.sub(re.compile('\[([a-zA-Z\'\’,\-]{1,50})(\s[0-9a-zA-Z\’\'’“”\(\),;:\.\?!\-\–#{4}]{1,50})*\.*\]', re.UNICODE), "", sp)
-        sp = re.sub(re.compile('\[Blackout \/ Opening credits\]', re.UNICODE), "", sp)
-        sp = re.sub(re.compile('\/ BLACKOUT', re.UNICODE), "", sp)
+		speaker = speaker.group()
+		speaker = re.sub('\A\s*','', speaker)
+		#speaker = re.sub('\s*[\.:]','', speaker).lower()
+		speaker = re.sub('\s*\([^)]+\)\s*','', speaker)
+		#if speaker in names_map:
+		#	speaker = names_map[speaker]
+		speech_ = re.sub('\A\s*[^:\.]+\s*:\s*','', line)
 
-        word = re.split('####', sp)
-        ok = 0
-        for w in word:
-            t1 = T()
-            t2 = T()
-            if ok == 1:
-                if re.match("(.*)</div>(.*)", w):
-                    w = re.sub(re.compile('</div>(.*)</table>', re.UNICODE), "", w)
-
-                if re.match("(.*): (.*)", w):
-                    ligne = re.split(': ', w)
-                elif re.match("[A-Z][a-z]{1,15}(.*) : (.*)", w):
-                    ligne = re.split(' : ', w)
-                elif re.match("[A-Z]{1,15}\s\(\w{1,15}(\s\w{1,15})*\.*\)(.*)", w):
-                    ligne = re.split('\) ', w)
-                    ligne[0] = ligne[0] + ")"
-                elif re.match("[A-Z]{1,15}\s(.*)", w):
-                    ligne = re.split(' ', w)
-
-                uligne0 = ligne[0].decode("utf-8")
-                ligne0 = uligne0.encode("ascii", "ignore")
-                uligne1 = ligne[1].decode("utf-8")
-                ligne1 = uligne1.encode("ascii", "ignore")
-                g.add_edge(t1, t2, spk=ligne0, speech=ligne1)
-            if ok == 0 and (re.match("[a-zA-Z]{1,15}(\s[a-zA-Z]{1,15}){1,2} : (.*)", w) or re.match("[A-Z]{1,15}(\s[A-Z]{1,15})?: (.*)", w)):
-                ok = 1
-                if re.match("[A-Z]{1,15}: (.*)", w):
-                    ligne = re.split(': ', w)
-                elif re.match("[A-Z][a-z]{1,15}(.*) : (.*)", w):
-                    ligne = re.split(' : ', w)
-                elif re.match("[A-Z]{1,15}\s\((to|in\s)+(\s+[a-zA-Z]{1,15}){1,5}\)(.*)", w):
-                    ligne = re.split('\) ', w)
-                    ligne[0] = ligne[0] + ")"
-                elif re.match("[A-Z]{1,15}\s(.*)", w):
-                    ligne = re.split(' ', w)
-                else:
-                    print w
-
-                uligne0 = ligne[0].decode("utf-8")
-                ligne0 = uligne0.encode("ascii", "ignore")
-                uligne1 = ligne[1].decode("utf-8")
-                ligne1 = uligne1.encode("ascii", "ignore")
-                g.add_edge(t1, t2, spk=ligne0, speech=ligne1)
+                t1 = T()
+                t2 = T()
+                g.add_edge(t1, t2, spk=speaker, speech=speech_)
 
         return g
+
+
+
+
+
--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(
-    name='GameOfThrones',
+    name='TheBigBangTheory',
     version="1.0",
-    description="Game of Thrones plugin for TVD dataset",
-    author="Herve Bredin",
+    description="The Big Bang Theory plugin for TVD dataset",
+    author="Anindya Roy",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["tvd>=0.2"],
     entry_points="""
         [tvd.series]
-        GameOfThrones=GameOfThrones:GameOfThrones
+        TheBigBangTheory=TheBigBangTheory:TheBigBangTheory
     """
 )
